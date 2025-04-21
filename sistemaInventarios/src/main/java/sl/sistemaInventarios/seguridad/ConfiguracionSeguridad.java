d76f242cc5d6cf7a0019c8eb325f1a963f050d8e
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-04-20 22:24:13 -0500

    new security configurations

--- sistemaInventarios/src/main/java/sl/sistemaInventarios/seguridad/ConfiguracionSeguridad.java
+++ sistemaInventarios/src/main/java/sl/sistemaInventarios/seguridad/ConfiguracionSeguridad.java
@@ -2,13 +2,26 @@ package sl.sistemaInventarios.seguridad;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionSeguridad {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())// Deshabilita CSRF para APIs
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/public/**")// Rutas públicas
+                        .permitAll().anyRequest().authenticated()) // El resto requiere autenticación
+                .httpBasic(Customizer.withDefaults());// Autenticación básica (usuario y contraseña en el header)
+        return http.build();
+
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
